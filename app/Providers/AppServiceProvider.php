d5683cdba7f885a22c779248ad4e909ffadd5c13
dacinti <dacinti@regionalmanagement.com>
2026-08-24 16:03:26 -0400

    Force HTTPS URLs on Render host

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    if ($this->app->environment('production')) {
+    if ($this->app->environment('production') || str_ends_with((string) request()->getHost(), '.onrender.com')) {
       URL::forceScheme('https');
     }
 
